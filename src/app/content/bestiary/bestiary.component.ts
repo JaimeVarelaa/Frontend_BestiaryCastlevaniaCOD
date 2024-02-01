522852d8103dbb549ac525c8fd9f414636125441
JaimeVarelaa <jaimeadolfov31@gmail.com>
2024-02-01 13:46:21 -0600

    Update display data

--- src/app/content/bestiary/bestiary.component.ts
+++ src/app/content/bestiary/bestiary.component.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+const enemy = 'http://localhost:3000/enemy';
+
 @Component({
   selector: 'app-bestiary',
   templateUrl: './bestiary.component.html',
   styleUrl: './bestiary.component.css',
 })
 export class BestiaryComponent {
-  beast: any;
+  enemy: any;
+  materials: any;
 
   constructor(private httpClient: HttpClient) {}
 
@@ -16,12 +19,10 @@ export class BestiaryComponent {
   }
 
   getData() {
-    const url = 'http://localhost:3000/enemy';
-
-    this.httpClient.get(url).subscribe(
+    this.httpClient.get(enemy).subscribe(
       (response: any) => {
-        this.beast = response;
-        console.log(this.beast);
+        this.enemy = response;
+        //console.log(this.enemy);
       },
       (error) => {
         console.log('Error al obtener datos.', error);
